9b253f3f4bec08deef7d0ba62a4bcda223c11e1f
MochKrzysztof <krzysmoch.programs@gmail.com>
2022-03-19 12:37:46 +0100

    create navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import { createNativeStackNavigator, NativeStackNavigationOptions } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-const App = () => {
-  return (
-    <View style={{ flex: 1 }}>
-      <Text>HELLO</Text>
-    </View>
-  );
-};
+import MainScreen from './src/screens/MainScreen/MainScreen';
+import SettingsScreen from './src/screens/SettingsScreen/SettingsScreen';
+import IconButton from './src/components/SettingsButton/SettingsButton';
+
+const Navigation = createNativeStackNavigator();
+
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <NavigationContainer>
+      <Navigation.Navigator initialRouteName='MainScreen'>
+        <Navigation.Screen name='MainScreen' component={MainScreen} options={mainScreenOptions} />
+        <Navigation.Screen name='SettingsScreen' component={SettingsScreen} options={settingsScreenOptions} />
+      </Navigation.Navigator>
+    </NavigationContainer>
+  </View>
+);
+
+// ------------------------- Screen Options -------------------------
+
+const mainScreenOptions: NativeStackNavigationOptions = {
+  title: 'Home',
+  headerTitleAlign: 'center',
+  headerRight: () => <IconButton name='ios-settings-sharp' size={32} style={{}} navigateTo='SettingsScreen' />
+}
+
+const settingsScreenOptions: NativeStackNavigationOptions = {
+  title: 'Settings',
+  headerTitleAlign: 'center',
+}
 
 export default App;
 
---
+++ src/screens/MainScreen/MainScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>MainScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default MainScreen;
