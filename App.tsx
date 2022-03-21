5b8d63808bb0de88859a8540a234afad07648e1e
Krzysztof Moch <83216158+MochKrzysztof@users.noreply.github.com>
2022-03-21 16:15:37 +0100

    Revert "Publishing preview version (not connection with API yet)"

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import React from 'react';
-import { View } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Provider } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import {View} from "react-native";
 
-import store from './src/redux/store'
-import MainNavigation from './src/navigation/MainNavigation';
-
-const App = () => (
-  <View style={{ flex: 1 }}>
-    <Provider store={store}>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <MainNavigation />
-      </GestureHandlerRootView>
-    </Provider>
-  </View>
-);
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}></View>
+  );
+};
 
 export default App;
 
--- src/common/colors.ts
+++
@@ -1,8 +0,0 @@
-const APP_COLORS = {
-    white: '#fff',
-    black: '#16161e',
-    lightNightBlue: '#4e5172',
-    nightBlue: '#34364c',
-}
-
-export default APP_COLORS
