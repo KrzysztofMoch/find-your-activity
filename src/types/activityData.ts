197deb640319a849e799b5567da706813c34acec
MochKrzysztof <krzysmoch.programs@gmail.com>
2022-03-24 14:47:38 +0100

    create type for activityData

---
+++ src/types/activityData.ts
@@ -0,0 +1,12 @@
+import activity from "./activity";
+
+type activityData = {
+    activity: string,
+    accessibility: number,
+    activityType: activity|'',
+    participants: number,
+    price: number,
+    key: number,
+}
+
+export default activityData;
