371df8f665a6d95801520fcc1e83e69ed3ae1d0b
MochKrzysztof <krzysmoch.programs@gmail.com>
2022-03-25 23:07:21 +0100

    update redux file

--- src/redux/optionsSlice.ts
+++ src/redux/optionsSlice.ts
@@ -1,27 +1,51 @@
 /* eslint-disable no-param-reassign */
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import activity from "../types/activity"
+import searchOption from "../types/seacrhOption"
 
-interface SliceType {
-  accessibility: number,
-  activityType: Array<activity>,
+interface OptionsSliceType {
+  accessibilityMax: number,
+  accessibilityMin: number,
+  priceMax: number,
+  priceMin: number,
   participants: number,
-  price: number,
+  activityType: Array<activity>,
+  searchOption: searchOption,
 }
 
 const initialState = { 
-  accessibility: 0,
-  activityType: ["recreational"],
-  participants: 0,
-  price: 0,
-} as SliceType
+  accessibilityMax: 1,
+  accessibilityMin: 0,
+  priceMax: 1,
+  priceMin: 0,
+  participants: 1,
+  activityType: ['recreational'],
+  searchOption: searchOption.ACTIVITY_TYPE
+} as OptionsSliceType
 
 const optionsSlice = createSlice({
   name: "options",
   initialState,
   reducers: {
     setAccessibility(state, action: PayloadAction<number>) {
-      state.accessibility = action.payload;
+      state.accessibilityMax = action.payload;
+      state.accessibilityMin = 0;
+    },
+    setAccessibilityMax(state, action: PayloadAction<number>) {
+      state.accessibilityMax = action.payload;
+    },
+    setAccessibilityMin(state, action: PayloadAction<number>) {
+      state.accessibilityMin = action.payload;
+    },
+    setPrice(state, action: PayloadAction<number>){
+      state.priceMax = action.payload;
+      state.priceMin = 0;
+    },
+    setPriceMax(state, action: PayloadAction<number>){
+      state.priceMax = action.payload;
+    },
+    setPriceMin(state, action: PayloadAction<number>){
+      state.priceMin = action.payload;
     },
     setActivityType(state, action: PayloadAction<Array<activity>>){
       state.activityType = action.payload;
@@ -29,16 +53,22 @@ const optionsSlice = createSlice({
     setParticipants(state, action: PayloadAction<number>){
       state.participants = action.payload;
     },
-    setPrice(state, action: PayloadAction<number>){
-      state.price = action.payload;
+    setSearchOption(state, action: PayloadAction<number>) {
+      state.searchOption = action.payload;
     },
   }
 })
 
 export const {
-  setAccessibility, 
-  setActivityType, 
-  setParticipants, 
+  setAccessibility,
+  setAccessibilityMax,
+  setAccessibilityMin,
   setPrice,
+  setPriceMax, 
+  setPriceMin,  
+  setActivityType, 
+  setParticipants,
+  setSearchOption,
 } = optionsSlice.actions
+export type { OptionsSliceType }
 export default optionsSlice.reducer
